9db08c95124f67067967a1ce96550cedaceb968e
Anri Liy <amedamachip713@gmail.com>
2021-02-21 12:16:31 -0800

    Templates

--- myvenv/src/pages/views.py
+++ myvenv/src/pages/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
   print(args,kwargs)
   print(request.user)
-  return HttpResponse("<h1>Hello World</h1>")
+  return render(request, "home.html", {})
 
-def contact_view(*args, **kwargs):
-  return HttpResponse("<h1>Contact Page</h1>")
+def contact_view(request, *args, **kwargs):
+  return render(request, "contact.html", {})
