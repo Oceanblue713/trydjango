97352ed555fe13c3a41427757d310a652f0f99ab
Anri Liy <amedamachip713@gmail.com>
2021-02-23 18:30:00 -0800

    For Loop, Template

--- myvenv/src/pages/views.py
+++ myvenv/src/pages/views.py
@@ -10,4 +10,11 @@ def contact_view(request, *args, **kwargs):
   return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-  return render(request, "about.html", {})
+  my_context = {
+    "title": "This is about",
+    "this_is_true": True,
+    "my_number": 123,
+    "my_list": [123, 234, 456, 321, "ABC"],
+    "my_html": "<h1>Hello world</h1>"
+  }
+  return render(request, "about.html", my_context)
