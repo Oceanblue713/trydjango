400f2e8cfac814356337c486787a461cc44474af
Anri Liy <amedamachip713@gmail.com>
2021-03-14 11:46:36 -0700

    Add validation to view

--- myvenv/src/products/views.py
+++ myvenv/src/products/views.py
@@ -4,19 +4,19 @@ from .models import Product
 
 # Create your views here.
 
-def product_create_view(request):
-  my_form = RawProductForm()
-  if request.method == "POST":
-    my_form = RawProductForm(request.POST)
-    if my_form.is_valid():
-      print(my_form.cleaned_data)
-      Product.objects.create(**my_form.cleaned_data)
-    else:
-      print(my_form.errors)
-  context = {
-    "form": my_form
-  }
-  return render(request, "products/product_create.html", context)
+# def product_create_view(request):
+#   my_form = RawProductForm()
+#   if request.method == "POST":
+#     my_form = RawProductForm(request.POST)
+#     if my_form.is_valid():
+#       print(my_form.cleaned_data)
+#       Product.objects.create(**my_form.cleaned_data)
+#     else:
+#       print(my_form.errors)
+#   context = {
+#     "form": my_form
+#   }
+#   return render(request, "products/product_create.html", context)
 
 # def product_create_view(request):
 #   if request.method == "POST":
@@ -25,17 +25,17 @@ def product_create_view(request):
 #   context = {}
 #   return render(request, "products/product_create.html", context)
 
-# def product_create_view(request):
-#   form = ProductForm(request.POST or None)
-#   if form.is_valid():
-#     form.save()
-#     form = ProductForm()
+def product_create_view(request):
+  form = ProductForm(request.POST or None)
+  if form.is_valid():
+    form.save()
+    form = ProductForm()
 
-#   context = {
-#     'form': form
-#   }
+  context = {
+    'form': form
+  }
 
-#   return render(request, "products/product_create.html", context)
+  return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
   obj = Product.objects.get(id=1)
